d5f772bcbf008924400d439fa746292319e2856f
Zi-ang Cao <ziangcao@stanford.edu>
2023-12-03 17:34:15 -0800

    re-organize

---
+++ private_support_code/fast-detect-gpt/run_GCP_0.sh
@@ -0,0 +1,2 @@
+python3 GPT_A1.py --InStepScore 0
+python3 GPT_A1.py --InStepScore 0 --total_timesteps 3E5
---
+++ private_support_code/fast-detect-gpt/run_GCP_1.sh
@@ -0,0 +1,2 @@
+python3 GPT_A1.py --InStepScore 1
+python3 GPT_A1.py --InStepScore 1 --total_timesteps 3E5
